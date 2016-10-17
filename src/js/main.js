a7487376b7f32c29b2316270799f0e29ae214fac
Tai Chen <mindlikewater@gmail.com>
2016-10-17 18:19:24 -0400

    Wrote some functions to create html form lines

--- src/js/main.js
+++ src/js/main.js
@@ -2,32 +2,39 @@ import $ from 'jquery';
 
 //variable holds HTML div class="container"
 var container = $(".container");
-//variable represents first object in the JSON array for the form
-var objectId = 0;
 
+//function to show error when attempting to request data
 function handleError (req) {
   console.log("Error: request failed");
   console.log(req);
 };
 
-function makeField () {
+//makes the HTML for a field in the form
+function makeField (data) {
   return `
-    <div class="fields">
-      ${formFields.label}
+    <div class="fields" id="${data.id}">
+      <i class="fa ${data.icon}"</i>
+      <input type="${data.type}" placeholder="${data.label}"/>
     </div>`;
-}
+};
 
-function addName (data) {
-  var nameHTML = nameField(data);
-  firstName.append(nameHTML);
-}
+function displayFields (data) {
+  //array holds all the form fields
+  var totalFields = [];
+  //loop through each field obj in the Form array
+  for (var i = 0; i < data.length; i++) {
+    var fieldHTML = makeField(data[i]);
+    totalFields.push(fieldHTML);
+  }
+  console.log(totalFields);
+  return $(".container").html(totalFields);
+};
 
-// var formFields = $(".formFields");
-// function getFields (event) {
-//   var field = $.ajax({
-//     url: `http://json-data.herokuapp.com/forms/${formFields}`
-//     success: addField,
-//     error: handleError
-//   });
-//   console.log(field);
-// }
+function getFields() {
+  var field = $.ajax({
+    url: `http://json-data.herokuapp.com/forms/`,
+    success: displayFields,
+    error: handleError
+  });
+  console.log(field);
+};
