e5e11e402fa35ce40c41732a5b4f8058a0fd3d8e
Tai Chen <mindlikewater@gmail.com>
2016-10-19 00:17:13 -0400

    Tried to modify function to make different div for the Languages drop-down. Not working yet.

--- src/js/main.js
+++ src/js/main.js
@@ -13,31 +13,80 @@ function handleError (req) {
 function makeField (data) {
   return `
     <div class="fields" id="${data.id}">
-      <i class="fa ${data.icon}"</i>
       <input type="${data.type}" placeholder="${data.label}"/>
+      <i class="fa ${data.icon}"</i>
     </div>`;
 };
 
-//creates all the fields that make up the form
-function displayFields (data) {
-  //array holds all the form fields
-  // var totalFields = [];
-  //loop through each field obj in the Form array
-  for (var i = 0; i < data.length; i++) {
-    var fieldHTML = makeField(data[i]);
-    // totalFields.push(fieldHTML);
-    $(".container").append(fieldHTML);
+/*function extracts the Language options from the data
+function extractLanguages (data) {
+  if (data.id === "user-language") {
+    var label = data.options.label;
+    var value = data.options.value;
 
+    return {
+      label: label,
+      value: value
+    };
+  };
+}; */
+
+//fuction makes the <Select> language menu
+function makeLanguageMenu (data) {
+  if (data.id === "user-language") {
+    var langList = data.map(function (x) {
+      return x.options;
+    });
   }
+  langList.forEach(function (x) {
+    return `
+    <div class="fields" id="${x.id}">
+      <select name="select">
+        <option value="${x.value}">${x.label}</option>
+      </select>
+      <i class="fa ${x.icon}"</i>
+    </div>`;
+  });
 };
 
+//makes the HTML for the submit button at the bottom of the form
+function submitButton() {
+  var buttonHTML = `
+    <div id="submit">
+      <button name="button">Submit Form</button>
+    </div>`;
+
+  //adds the button to the bottom of the container
+  $(".container").append(buttonHTML);
+};
+
+//creates all the fields that make up the form
+function allFields (data) {
+  //loop through each field obj in the Form array
+  for (var i = 0; i < data.length; i++) {
+    if (data.id === "user-language") {
+      makeLanguageMenu(data);
+    }
+    else {
+      var fieldHTML = makeField(data[i]);
+    }
+    //add all the HTML fields to the div, container
+    $(".container").append(fieldHTML);
+  }
+  //call function that creates HTML for Submit button
+  submitButton();
+};
+
+//gets the data from the url
 function getFields() {
   $.ajax({
     url: `http://json-data.herokuapp.com/forms/`,
-    success: displayFields,
+    success: allFields,
     error: handleError
   });
 };
 
-var inputbar = document.getElementsByClassName("container");
-inputbar.innerHTML = getFields();
+//gets location on the page where the form fields should go
+var inputbars = document.getElementsByClassName("container");
+//puts the form fields onto the webpage
+inputbars.innerHTML = getFields();
